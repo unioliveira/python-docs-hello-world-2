80d9359cc794383c11bfcd3bd0da85438864a38a
unioliveira <71647799+unioliveira@users.noreply.github.com>
2024-02-04 13:07:27 -0300

    Update test_anyname.py

--- test_anyname.py
+++ test_anyname.py
@@ -1,5 +1,11 @@
-def func(a):
-	return a - 1
+#def func(a):
+#	return a - 1
 
-def test_testmethod():
-	assert func(6) == 5
+#def test_testmethod():
+#	assert func(6) == 5
+
+def pytest_collection_modifyitems(items):
+    test_name = 'test_1'
+    test_index = next((i for i, item in enumerate(items) if item.name == test_name), -1)
+    test_item = items.pop(test_index)
+    items.append(test_item)
